4fc0c9f14bbab07a8f4560498c481f13e038326c
ionakathryn <ionakathryn123@googlemail.com>
2016-09-23 14:58:17 +0100

    Update Main.java

--- src/net.cardiffdev/Main.java
+++ src/net.cardiffdev/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hiya!");
+        System.out.println("Hope I don't break this...:P");
+        System.out.println("Where should I be editing???");
 
     }
     
